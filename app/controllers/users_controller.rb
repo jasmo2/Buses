7e64da003302a4091515d22b643cb085a2bbb715
jasmo2 <jasmo2.com@gmail.com>
2015-05-05 10:05:39 -0500

    handle Diferent login cases

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,11 +1,12 @@
 class UsersController < ApplicationController
-	before_action :authenticate_user!, :except => [:index]
+	before_action :authenticate_user!, except: [:index]
 	def index
 		unless user_signed_in?
 			render "index"
 		else
 			case current_user.role
 			when "admin"
+				render "dashboard"
 			when "editor"
 				if cookies[:checkpoint]
 					redirect_to controller: "records", action: "new"
@@ -13,13 +14,13 @@ class UsersController < ApplicationController
 					render :checkpoint
 				end
 			when "reader"
+					redirect_to controller: "bus_routes", action: "new"
 			end
-			
 		end
 	end
 
 	def checkpoint
-		cookies.permanent[:checkpoint] = checkpoint_params[:checkpoint]  
+		cookies.permanent[:checkpoint] = checkpoint_params[:checkpoint]
 		redirect_to controller: "records", action: "new"
 	end
 	private
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,21 @@
 module ApplicationHelper
+  def header_role(role)
+    case role
+    when "admin"
+      return "Administrador"
+    when "editor"
+      return "Editor"
+    when "reader"
+      return "Lector"
+    end
+  end
+  def navbar
+    if  user_signed_in?
+      if cookies[:checkpoint]
+        render "layouts/header"
+      elsif current_user.reader? || current_user.admin?
+        render "layouts/header"
+      end
+    end
+  end
 end
