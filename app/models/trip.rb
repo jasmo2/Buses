982623c8cff6d17269c6e6eba97e68ca7e1c99cb
jasmo2 <jasmo2.com@gmail.com>
2015-05-06 13:45:49 -0500

    #1 prepare Trip model to get time from excel

--- app/models/trip.rb
+++ app/models/trip.rb
@@ -1,7 +1,28 @@
+# == Schema Information
+#
+# Table name: trips
+#
+#  id             :integer          not null, primary key
+#  direction      :integer
+#  operation_date :date
+#  bus_id         :integer
+#  bus_routes_id  :integer
+#  neighbourhood  :string
+#  created_at     :datetime         not null
+#  updated_at     :datetime         not null
+#  start_time     :time
+#
+
 class Trip < ActiveRecord::Base
   belongs_to :bus
   belongs_to :bus_route
   has_many :records
   enum direction: [:left,:right]
+  validates :start_time, numericality: true
+  after_validation :change_to_date
 
+  private
+  def change_to_date
+    self.start_time = Time.at(self.start_time).utc.strftime("%H:%M")
+  end
 end
