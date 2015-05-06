1bc70a380394cc96c68f6740f50bc01b543911f8
jasmo2 <jasmo2.com@gmail.com>
2015-05-05 20:51:56 -0500

    #1 Custom seach input field

--- app/assets/javascripts/bus_routes.js
+++ app/assets/javascripts/bus_routes.js
@@ -8,4 +8,17 @@ $(document).ready(function() {
         bLengthChange: false, bFilter: false, bInfo: false
       }
     );
+    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
+           var input = $(this).parents('.input-group').find(':text'),
+               log = numFiles > 1 ? numFiles + ' files selected' : label;
+           if( input.length ) {
+               input.val(log);
+           }
+       });
+    $(document).on('change', '.btn-file :file', function() {
+        var input = $(this),
+            numFiles = input.get(0).files ? input.get(0).files.length : 1,
+            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+        input.trigger('fileselect', [numFiles, label]);
+    });
 } );
