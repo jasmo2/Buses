4d1392e534574a063002ee5e11fb7247b966de74
jasmo2 <jasmo2.com@gmail.com>
2015-05-09 12:04:09 -0500

    #1 fully functional for Admin and basic for Reader

--- app/controllers/bus_routes_controller.rb
+++ app/controllers/bus_routes_controller.rb
@@ -1,6 +1,19 @@
 class BusRoutesController < ApplicationController
   before_action :role_editor
+  before_action :not_admin, only: [:import]
   def index
+    if current_user.admin?
+      @bus_routes = BusRoute.all
+      unless @bus_routes.empty?
+        @counter = @bus_routes.max_by { |bus_route| bus_route.trips.count }
+        @counter = @counter.trips.count
+      else
+        @counter = 1
+      end
+      render "index"
+    else
+      
+    end
   end
 
   def import
@@ -8,13 +21,14 @@ class BusRoutesController < ApplicationController
       BusRoute.import(params[:file])
       flash[:notice] = "Los archivos se han cargado correctamente"
     rescue ArgumentError => e
-      puts "~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~"
-      puts "Exception catched"
-      puts e
-      puts ""
       flash[:alert] = e.message
     end
       redirect_to action: "index", notice: "Rutas importadas"
   end
- 
+ private
+ def not_admin
+  unless current_user.admin?
+    redirect_to action: "index"
+  end
+ end
 end
