f6026e3bff7bcc145bc4a2fd6134488f8e5dd5b8
jasmo2 <jasmo2.com@gmail.com>
2015-05-08 22:14:39 -0500

    #1 improve table aparence

--- app/helpers/bus_routes_helper.rb
+++ app/helpers/bus_routes_helper.rb
@@ -1,2 +1,18 @@
 module BusRoutesHelper
+  def flash_import(flash)
+    if !flash.blank?
+      alert_type = ""
+      color = ""
+      if flash[:notice]
+        alert_type = flash[:notice]
+        color = "success"
+      elsif flash[:alert]
+        alert_type = flash[:alert]
+        color = "danger"
+      end
+      return "<div class=\"col-xs-offset-1 col-xs-8 alert alert-#{color}\" role=\"alert\">
+              #{alert_type}
+            </div>".html_safe
+    end
+  end
 end
