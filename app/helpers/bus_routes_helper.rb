9a7a7cfb43bb0acaaf570718e7b9765efb3658c2
jasmo2 <jasmo2.com@gmail.com>
2015-05-09 12:05:36 -0500

    #1 table_routes fetch

--- app/helpers/bus_routes_helper.rb
+++ app/helpers/bus_routes_helper.rb
@@ -15,4 +15,57 @@ module BusRoutesHelper
             </div>".html_safe
     end
   end
+  def table_routes(bus_routes,counter = 0)
+    if bus_routes.empty?
+      return '<tr>
+        <td>No hay datos que mostrar</td>
+        <td>
+          <table class="inner-table">
+            <tbody>
+             <td>-</td>
+             <td>00:00</td>
+             <td>-</td>
+           </tbody>
+         </table>
+       </td>
+      </tr>'.html_safe
+    else
+      html = ""
+      bus_routes.each do |bus_route| 
+        html += %Q[ <tr>
+          <td>#{bus_route.name}</td> ]
+        td_index = 0
+        bus_route.trips.each do |trip|
+          html += %Q[
+            <td>
+              <table class="inner-table">
+                <tbody>
+                 <td>#{direccion = trip.direction == "left" ? "I" : "D"}</td>
+                 <td>#{trip.start_time.strftime "%I:%M:%S %p"}</td>
+                 <td>#{trip.neighbourhood}</td>
+               </tbody>
+             </table>
+           </td>
+          ]
+          td_index += 1
+        end
+        while td_index  < counter
+          html += %Q[
+            <td>
+              <table class="inner-table">
+                <tbody>
+                 <td> - </td>
+                 <td> - </td>
+                 <td> - </td>
+               </tbody>
+             </table>
+           </td>
+          ]
+          td_index += 1
+        end
+        html +=  "</tr>"
+      end
+      return html.html_safe
+    end
+  end
 end
