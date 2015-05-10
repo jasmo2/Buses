20258c8c50f384dd157a54d82429535693c391ca
jasmo2 <jasmo2.com@gmail.com>
2015-05-10 16:33:43 -0500

    #2 refactor js validations

--- app/assets/javascripts/bus_routes.js
+++ app/assets/javascripts/bus_routes.js
@@ -1,27 +1,12 @@
   // Place all the behaviors and hooks related to the matching controller here.
   // All this logic will automatically be available in application.js.
   // You can use CoffeeScript in this file: http://coffeescript.org/
-$(document).ready(function() {
-    // $.fn.dataTableExt.sErrMode = 'throw';
+$(document).on('ready page:load', function () {
+  // Actions to do
     $('#bus-route-table').DataTable(
       {
         bLengthChange: false, bFilter: false, bInfo: false
       }
     );
-    $('.btn-file :file').on('fileselect', function(event, numFiles, label) {
-           var input = $(this).parents('.input-group').find(':text'),
-               log = numFiles > 1 ? numFiles + ' files selected' : label;
-           if( input.length ) {
-               input.val(log);
-           }
-       });
-    $(document).on('change', '.btn-file :file', function() {
-        var input = $(this),
-            numFiles = input.get(0).files ? input.get(0).files.length : 1,
-            label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
-        input.trigger('fileselect', [numFiles, label]);
-    });
-    $('.btn.btn-primary').on('click', function () {
-      $('body').modalmanager('loading');
-    })
+    validationFile();
 } );
