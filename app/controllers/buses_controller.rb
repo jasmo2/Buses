9cd9dc2da6a615d39aa6efdc678e439421bc004d
jasmo2 <jasmo2.com@gmail.com>
2015-05-11 16:22:44 -0500

    #2 handle errors and show friendly to the user

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,3 +1,17 @@
 class BusesController < ApplicationController
-    
+  before_action :role_editor
+  before_action :not_admin, only: [:import]
+
+  def index
+    render :index
+  end
+  def import
+    begin
+      Bus.add_routes(params[:file])
+      flash[:notice] = "Los archivos se han cargado correctamente"
+    rescue ArgumentError => e
+      flash[:alert] = e.message
+    end
+      redirect_to action: "index", notice: "Rutas importadas"
+  end       
 end
