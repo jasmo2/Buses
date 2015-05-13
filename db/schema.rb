88ecea3ca1e6c96bba209bc662360a02ea6b5858
jasmo2 <jasmo2.com@gmail.com>
2015-05-12 20:26:45 -0500

    delete unecessary columns in buses

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150509034527) do
+ActiveRecord::Schema.define(version: 20150513002108) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,8 +23,6 @@ ActiveRecord::Schema.define(version: 20150509034527) do
   end
 
   create_table "buses", force: :cascade do |t|
-    t.string   "create"
-    t.string   "update"
     t.string   "plate_license"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
