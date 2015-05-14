b1a977d5fad461a106612e54f2b0b3ebd838080c
jasmo2 <jasmo2.com@gmail.com>
2015-05-13 23:48:54 -0500

    #3 Client validation

---
+++ app/assets/javascripts/formValidation.js
@@ -0,0 +1,35 @@
+formValidation = function () {
+
+  $("#bus-form").validate({
+    rules: {
+      "bus[id]": {
+          required: true,
+          number: true
+        },
+      "bus[plate_license]": {
+        required: true,
+        minlength: 6
+      },
+      "bus[driver_name]": {required: true}
+    },
+    messages: {
+      "bus[id]": {
+        number: "Ingresar un número",
+        required: "Campo requerido"
+      },
+      "bus[plate_license]": {
+        rangelength: "Ingresar una placa de 6 caracteres sin espacio",
+        required: "Campo requerido"
+      },
+      "bus[driver_name]": {
+        required: "Ingresar nombre"
+      }
+    },
+        
+    submitHandler: function(form) {
+      debugger
+        form.submit();
+    }
+  });
+
+}
