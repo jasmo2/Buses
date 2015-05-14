16473fbf2d77195074563de2e2f8eb19c284a6dc
jasmo2 <jasmo2.com@gmail.com>
2015-05-13 23:52:49 -0500

    #3 add new bus

--- app/controllers/buses_controller.rb
+++ app/controllers/buses_controller.rb
@@ -1,10 +1,35 @@
 class BusesController < ApplicationController
   before_action :role_editor
+  before_action :new_bus, only: [:new]
   before_action :not_admin, only: [:import]
 
   def index
-    render :index
+    render "index"
   end
+
+  def list_buses
+    
+  end
+
+  def new
+  end
+
+  def create
+    @bus = Bus.new(bus_params)
+
+    respond_to do |format|
+       if @bus.save
+          flash[:notice] = 'El bus fué creado exitosamente'
+          format.html {  redirect_to  action: "list_buses" }
+          format.json
+       else   
+          flash[:alert] = 'no se ah podido guardar el autobus'
+          format.html { redirect_to action: 'list_buses'}
+          format.json { render json: @bus.errors, status: :unprocessable_entity }
+       end
+     end
+  end
+
   def import
     begin
       Bus.add_routes(params[:file])
@@ -13,5 +38,12 @@ class BusesController < ApplicationController
       flash[:alert] = e.message
     end
       redirect_to action: "index", notice: "Rutas importadas"
-  end       
+  end  
+  private
+  def new_bus
+    @bus = Bus.new
+  end
+  def bus_params
+    params.require(:bus).permit(:id,:plate_license,:driver_name)
+  end     
 end
