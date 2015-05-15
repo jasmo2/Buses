371325ae2b1b81d5cd0b992cf277c50862a18bba
jasmo2 <jasmo2.com@gmail.com>
2015-05-14 23:29:03 -0500

    #3 multi-assignment route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # resources :users
 
   
-  devise_for :users 
+  devise_for :users #, controllers: {registrations: 'users/registrations'}
   root 'users#index'
   
   authenticate :user do
@@ -15,10 +15,20 @@ Rails.application.routes.draw do
       collection { post :import }
     end
     get '/autobuses' => 'buses#list_buses'
+
   end
   post '/checkpoint' => 'users#checkpoint' ,as: :user_checkpoint
+  get '/usuarios' => 'users#list_users'
+  scope "/coonatra" do
+    resources :users, except: [:index,:show]
+    resources :users, only: [] do
+      collection do
+        get ':id/autobuses' => 'users#bus_list' ,as: :bus_list
+        patch 'autobuses' => 'users#bus_assignment' ,as: :bus_assignment
+      end
+    end
 
-
+  end
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
