a83d43bc5a3f5d08afd0fd13e9a9e58e825ee844
jasmo2 <jasmo2.com@gmail.com>
2015-05-25 18:12:06 -0500

    operationDate model and assosiation

--- app/models/operation_date.rb
+++ app/models/operation_date.rb
@@ -8,7 +8,11 @@
 #  bus_route_id   :integer
 #  created_at     :datetime         not null
 #  updated_at     :datetime         not null
+#  terminal       :boolean          default(TRUE)
+#  trip_column    :integer          default(0)
 #
 
 class OperationDate < ActiveRecord::Base
+  has_many :buses
+  has_many :bus_routes
 end
