fd72c709b0dd5dadd1891815fdeff3407c93cace
jasmo2 <jasmo2.com@gmail.com>
2015-05-29 11:17:52 -0500

    refactor Record

--- app/models/record.rb
+++ app/models/record.rb
@@ -27,16 +27,6 @@ class Record < ActiveRecord::Base
 
   private
 
-  def get_trip
-    if self.terminal?
-      working_day = OperationDate.where(operation_date: Time.now.in_time_zone(-5).to_date).take
-      working_day.trip_column += 1
-      working_day.save!
-    end
-    # BusRoute.where(bus_id: self.bus_id, operation_date:  Time.now.in_time_zone(-5).to_date).take
-    # trip = Trip.where(bus_id: self.bus_id, operation_date:  Time.now.in_time_zone(-5).to_date).take
-    self.trip_id = if trip != nil then trip.id else nil end
-  end
   def register_type_N_time
     if self.register_time == nil
       self.register_time = Time.now.in_time_zone(-5)
