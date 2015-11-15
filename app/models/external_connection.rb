aff1edc89402ffa1480926aefe95cacdbe413abc
jasmo2 <jasmo2.com@gmail.com>
2015-11-15 15:30:53 -0500

    add ExternalConnection to micro-service

---
+++ app/models/external_connection.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+class ExternalConnection
+    include ActiveModel::Serialization
+    attr_reader :args
+   def initialize(args={})
+     args[:fecha_f] = Time.parse(args[:fecha_f]).to_i
+     args[:fecha_i] = Time.parse(args[:fecha_i]).to_i
+     @args = args
+   end
+
+  def send_data
+    url = URI.parse("#{@args[:url]}/bus_id/#{@args[:bus_id]}/fecha_i/#{@args[:fecha_i]}/fecha_f/#{@args[:fecha_f]}")
+    req = Net::HTTP::Get.new(url, initheader = {'Content-Type' =>'application/json'})
+    res = Net::HTTP.start(url.host, url.port) {|http|
+      http.request(req)
+    }
+    return res.body
+  end
+end
