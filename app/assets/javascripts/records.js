0086c4724c99088e8b15964d0c4187ba25bdd81e
jasmo2 <jasmo2.com@gmail.com>
2015-11-16 21:58:24 -0500

    Refactor to jQuery ajax instead of Rails ajax for modal configuration

--- app/assets/javascripts/records.js
+++ app/assets/javascripts/records.js
@@ -8,6 +8,7 @@ $(document).on('ready page:load', function () {
 
     var targetDate =  $('.initial-date'),
         today = moment().toISOString();
+        $progresBar = $("#passenger_wrapper .progress");
 
     // format: "YYYY-MM-DD\THH:mm:ss\Z"
     targetDate.datetimepicker({
@@ -20,4 +21,17 @@ $(document).on('ready page:load', function () {
         btns: [ 'bus_id', 'bus_license_plate']
     });
     crossButtons.init();
+    $('form').submit(function() {
+        $progresBar.show();
+        this.disabled = true
+        var valuesToSubmit = $(this).serialize();
+        $.ajax({
+            type: this.method.toUpperCase(),
+            url: $(this).attr('action'), //sumbits it to the given url of the form
+            data: valuesToSubmit
+        }).success(function(json){
+            console.log("success", json);
+        });
+        return false; // prevents
+    })
 });
