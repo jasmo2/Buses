2095dd9f0fb749ca3f844827cfbf2a40e7900224
jasmo2 <jasmo2.com@gmail.com>
2015-11-17 19:54:56 -0500

    validate args[:url] has 'http://' string

--- app/models/external_connection.rb
+++ app/models/external_connection.rb
@@ -5,18 +5,15 @@ class ExternalConnection
    def initialize(args={})
      args[:fecha_f] = Time.parse(args[:fecha_f]+"T23:59:00.000").to_i
      args[:fecha_i] = Time.parse(args[:fecha_i]+"T03:00:00.000").to_i
+     args[:url] = (args[:url].include? "http://") ? args[:url] : "http://#{args[:url]}"
      @args = args
    end
 
   def send_data
     url = URI.parse("#{@args[:url]}/bus_id/#{@args[:bus_id]}/fecha_i/#{@args[:fecha_i]}/fecha_f/#{@args[:fecha_f]}")
     puts ""
-    puts "require 'net/http': #{require 'net/http'}"
-    puts "@args[:url] #{@args[:url]}"
-    puts "@args[:bus_id]}: #{@args[:bus_id]}"
-    puts "@args[:fecha_i]: #{@args[:fecha_i]}"
-    puts "@args[:fecha_f]: #{@args[:fecha_f]} "
     puts "url: #{url} "
+    puts "url.host: #{url.host} "
     puts ""
     begin
       req = Net::HTTP::Get.new(url, initheader = {'Content-Type' =>'application/json'})
