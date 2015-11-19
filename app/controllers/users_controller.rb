b278a9195e511e0090f658d313a00f5b1abb7103
jasmo2 <jasmo2.com@gmail.com>
2015-11-19 00:02:19 -0500

    user creation log register

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -33,7 +33,7 @@ class UsersController < ApplicationController
 	def create
 		@user = User.new(user_params)
 		if current_user.admin_save(@user)
-			puts "se ha creado el usuario #{@user.username} con id: #{user.id}"
+			puts "se ha creado el usuario #{@user.username} con id: #{@user.id}"
 			flash[:notice] = "Se ah creado el nuevo usuario #{@user.username}"
 			redirect_to action: "list_users"
 		else
